a23651273b4646b2a89b24855444adc6fa7f13be
Archenoth <archenoth@archenoth.com>
2013-09-07 22:16:29 +0000

    Added memory and command options.
    - Added maximum memory checker.
    - Added error command that runs when maximum memory is exceeded.

--- minecraft-command.pl
+++ minecraft-command.pl
@@ -3,17 +3,22 @@ use strict;
 use warnings;
 use Getopt::Std;
 
-#Defaults... Modify these as you see fit.
+##Defaults... Modify these as you see fit.
+#The name of the screen to try to connect to.
 my $screen = "minecraft";
 
 my %args;
-getopts('s:n:hSmq', \%args);
+getopts('s:n:E:M:hSmq', \%args);
 
 print "Minecraft Command, a command-line screen Minecraft server "
     . "administration tool.\n\n"
     . "Usage: $0 <arguments>\n"
     . "\t-h\t\t\tThis help screen.\n"
+    . "\t-E <command>\t\tThe command to run in the event of an error.\n"
     . "\t-m\t\t\tDisplays server memory usage\n"
+    . "\t-M <max memory (in KB)>\tThe process returns an error if the memory\n"
+    . "\t\t\t\tusage is larger than the specified value, also\n"
+    . "\t\t\t\truns a command specified by the -E parameter.\n"
     . "\t-s <message>\t\tSay a message to the server.\n"
     . "\t-S\t\t\tSaves the server map.\n"
     . "\t-q\t\t\tStops the server.\n"
@@ -21,10 +26,11 @@ print "Minecraft Command, a command-line screen Minecraft server "
     . "\t\t\t\t(Default is \"minecraft\")\n" and exit if defined $args{h};
 
 print "Use $0 -h for help...\n" and exit unless defined
-    $args{s} or $args{S} or $args{q} or $args{m};
+    $args{s} or $args{S} or $args{q} or $args{m} or $args{M};
 
 #Arguments
 $screen = $args{n} if defined $args{n};
+my $errorcommand = $args{E} if defined $args{E};
 
 sub mine_send
 {
@@ -46,3 +52,8 @@ print "Memory: " . check_mem() . "KB used...\n" if defined $args{m};
 mine_send("say $args{s}") if defined $args{s} && print "Saying $args{s}...\n";
 mine_send("save-all") if defined $args{S} && print "Saving server...\n";
 mine_send("stop") if defined $args{q} && print "Stopping server...\n";
+
+if(defined $args{M} and (check_mem() > $args{M})){
+    return 1 unless defined $errorcommand;
+    system($errorcommand);
+}
